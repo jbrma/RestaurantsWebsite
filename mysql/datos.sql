f695afb90b0319be28d2dd57dc8006b509444fb7
Jorge <jorgbr01@ucm.es>
2023-04-20 10:43:14 +0200

    base de datos

--- mysql/datos.sql
+++ mysql/datos.sql
@@ -1,25 +1,31 @@
 /*
   Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
 */
-TRUNCATE TABLE `RolesUsuario`;
-TRUNCATE TABLE `Roles`;
 TRUNCATE TABLE `Usuarios`;
-
-INSERT INTO `Roles` (`id`, `nombre`) VALUES
-(1, 'admin'),
-(2, 'user');
-
-
-INSERT INTO `RolesUsuario` (`usuario`, `rol`) VALUES
-(1, 1),
-(1, 2),
-(2, 2);
+TRUNCATE TABLE `Editores`;
+TRUNCATE TABLE `Categorias`;
+TRUNCATE TABLE `Restaurantes`;
 
 /*
   user: userpass
   admin: adminpass
 */
-INSERT INTO `Usuarios` (`id`, `nombreUsuario`, `nombre`, `password`) VALUES
-(1, 'admin', 'Administrador', '$2y$10$O3c1kBFa2yDK5F47IUqusOJmIANjHP6EiPyke5dD18ldJEow.e0eS'),
-(2, 'user', 'Usuario', '$2y$10$uM6NtF.f6e.1Ffu2rMWYV.j.X8lhWq9l8PwJcs9/ioVKTGqink6DG');
+INSERT INTO `Usuarios` (`username`, `password`, `nombre`,`correo`,`rol`) VALUES
+('user1', 'user1pass', 'Pedro', 'pedro@gmail.com', 'usuario'),
+('user2', 'user2pass', 'Maria', 'maria@gmail.com', 'usuario'),
+('admin', 'adminpass', 'Celia', 'admin@gmail.com', 'admin');
 
+INSERT INTO `Editores` (`id_editor`, `username`, `nombre`,`password`) VALUES
+(1, 'editor1', 'Editor 1', 'pass1'),
+(2, 'editor2', 'Editor 2', 'pass2'),
+(3, 'editor3', 'Editor 3', 'pass3');
+
+INSERT INTO `Categorias` (`id_categoria`, `nombre`, `clave`) VALUES
+(1, 'Mexicano', '1'),
+(2, 'Chino', '2'),
+(3, 'Italiano', '3');
+
+INSERT INTO `Restaurantes` (`id_rest`, `nombre`, `apertura`, `direccion`, `imagen`, `id_editor`) VALUES
+(1, 'Restaurante Mexicano', 1990, 'Calle 1, Ciudad', 'img/mexicano.jpg', 1),
+(2, 'Restaurante Chino', 1985, 'Calle 2, Ciudad', 'img/chino.jpg', 2),
+(3, 'Restaurante Italiano', 2000, 'Calle 3, Ciudad', 'img/italiano.jpg', 3);
--- mysql/estructura.sql
+++ mysql/estructura.sql
@@ -1,27 +1,42 @@
 /*
   Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
 */
-DROP TABLE IF EXISTS `RolesUsuario`;
-DROP TABLE IF EXISTS `Roles`;
 DROP TABLE IF EXISTS `Usuarios`;
-
-CREATE TABLE IF NOT EXISTS `Roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE IF NOT EXISTS `RolesUsuario` (
-  `usuario` int(11) NOT NULL,
-  `rol` int(11) NOT NULL,
-  PRIMARY KEY (`usuario`,`rol`),
-  KEY `rol` (`rol`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `Editores`;
+DROP TABLE IF EXISTS `Categorias`;
+DROP TABLE IF EXISTS `Restaurantes`;
 
 CREATE TABLE IF NOT EXISTS `Usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombreUsuario` varchar(30) COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
-  `password` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
+    `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL, 
+    `password` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
+    `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+    `correo` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+    `rol` varchar(15) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Usuario',
+    PRIMARY KEY (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Editores` (
+    `id_editor` INT (11) NOT NULL AUTO_INCREMENT,
+    `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+    `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+    `password` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
+   PRIMARY KEY (`id_editor`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Categorias` (  
+  `id_categoria` INT (11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `clave` varchar(70) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Restaurantes` (
+  `id_rest` INT (11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `apertura` INT (5) NOT NULL AUTO_INCREMENT,
+  `direccion` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `imagen` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_editor`  INT (11) NOT NULL,
+  PRIMARY KEY (`id_rest`),
+  FOREIGN KEY (`id_editor`) REFERENCES `Editores`(`id_editor`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
